0366488ba8327f9afe15deba352ffc28f1880bda
joonaeng <jylee33286315@gmail.com>
2025-01-09 12:30:08 +0900

    ìwork

---
+++ src/main/java/com/example/sbb_250109/Sbb250109Application.java
@@ -0,0 +1,13 @@
+package com.example.sbb_250109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb250109Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb250109Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbb_250109/Sbb250109ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb_250109;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb250109ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
